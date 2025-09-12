6feb52b3d6232232a4ca15ca51af7e44da3a8721
kumar saurabh <kumarsaurabh27d@gmail.com>
2025-09-13 00:37:54 +0530

    resolved some errors

--- pages/index.vue
+++ pages/index.vue
@@ -13,17 +13,3 @@
     <HeroCtaFooter />
   </div>
 </template>
-
-<script setup>
-import Navbar from '~/components/Navbar.vue'
-import HeroSection from '~/components/HeroSection.vue'
-import Trusted from '~/components/Trusted.vue'
-import WhatIs from '~/components/WhatIs.vue'
-import OpenFeatureSection from '~/components/OpenFeatureSection.vue'
-import ControlPower from '~/components/ControlPower.vue'
-import Features from '~/components/Features.vue'
-import ComingSoon from '~/components/ComingSoon.vue'
-import Growth from '~/components/Growth.vue'
-import UseIntelliToggle from '~/components/UseIntelliToggle.vue'
-import HeroCtaFooter from '~/components/HeroCtaFooter.vue'
-</script>
